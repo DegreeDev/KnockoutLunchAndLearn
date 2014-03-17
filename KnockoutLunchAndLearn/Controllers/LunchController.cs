552e43a18cc7309a1ceb4adcca4d34e047ee927d
Matthew Lee <mslee61@gmail.com>
2014-03-17 12:30:11 -0500

    commit
    ddd

---
+++ KnockoutLunchAndLearn/Controllers/LunchController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnockoutLunchAndLearn.Controllers
+{
+    public class LunchController : Controller
+    {
+        //
+        // GET: /Lunch/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
