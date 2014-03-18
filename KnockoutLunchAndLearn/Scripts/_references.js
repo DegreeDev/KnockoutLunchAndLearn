24915924f73e4f1d071e331681925a47bb25dfb0
Matthew Lee <mslee61@gmail.com>
2014-03-17 21:10:49 -0500

    updated datagrid

--- KnockoutLunchAndLearn/Scripts/_references.js
+++ KnockoutLunchAndLearn/Scripts/_references.js
@@ -1,10 +1,18 @@
+<<<<<<< HEAD
 ﻿/// <reference path="knockout.mapping-latest.js" />
 /// <reference path="knockout-3.0.0.debug.js" />
 /// <reference path="modernizr-2.7.1.js" />
+=======
+﻿/// <reference path="modernizr-2.7.1.js" />
+/// <reference path="knockout-3.0.0.debug.js" />
+>>>>>>> updated datagrid
 /// <reference path="jquery-ui-1.10.4.js" />
 /// <reference path="jquery-2.1.0.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="require.js" />
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> updated datagrid
