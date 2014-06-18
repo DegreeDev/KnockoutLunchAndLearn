8cc4a5b23e40aea60e204644884f79e9abf5c4d4
Matthew Lee <mslee61@gmail.com>
2014-06-18 09:52:34 -0500

    Added Serialization lesson

--- MappingPluginLesson/Controllers/HomeController.cs
+++ MappingPluginLesson/Controllers/HomeController.cs
@@ -4,30 +4,88 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MappingPluginLesson.Models;
+using MappingPluginLesson.Models.Serialization;
+using Newtonsoft.Json;
 
 namespace MappingPluginLesson.Controllers
 {
-	 public class HomeController : Controller
-	 {
-		  public ActionResult Index()
-		  {
-				return View();
-		  }
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+		//Part 0
+		public ActionResult Serialization()
+		{
+			return View();
+		}
 
-		  //Part 1
-		  public ActionResult Simple()
-		  {
-				return View();
-		  }
-		  [HttpGet]
-		  public JsonResult SimpleData()
-		  {
-				return Json(new TeamViewModel
+		public JsonResult Get()
+		{
+			return Json(new Person
+			{
+				Id = Guid.NewGuid(),
+				Name = "Matthew",
+				Mother = new Person
 				{
-					 Id = Guid.NewGuid(),
-					 CurrentReleaseName = "Black Sabbath",
-					 ProductName = "Sales Process Pro",
-					 TeamMembers = new List<TeamMemberViewModel>
+					Id = Guid.NewGuid(),
+					Name = "Jody"
+				},
+				Father = new Person
+				{
+					Id = Guid.NewGuid(),
+					Name = "Patrick"
+				}
+			}, JsonRequestBehavior.AllowGet);
+		}
+		public JsonResult Dictionary()
+		{
+			var result = new Dictionary<Guid, SimpleObject>();
+
+			var data = new List<SimpleObject>(){
+				new SimpleObject{ Id = Guid.NewGuid(), FathersName = "Gary", MothersName = "Jan", Name="Derek"},
+				new SimpleObject{ Id = Guid.NewGuid(), FathersName = "Murray", MothersName = "Dianne", Name="Eric"},
+				new SimpleObject{ Id = Guid.NewGuid(), FathersName = "Charles", MothersName = "Sue", Name="Justin"},
+				new SimpleObject{ Id = Guid.NewGuid(), FathersName = "Fred", MothersName = "Kathy", Name="Mike"}
+			};
+
+			result = data.ToDictionary(x => x.Id, x => x);
+
+			return Json(result, JsonRequestBehavior.AllowGet);
+		}
+		public string DictionaryJsonNet()
+		{
+			var result = new Dictionary<Guid, SimpleObject>();
+
+			var data = new List<SimpleObject>(){
+				new SimpleObject{ Id = Guid.NewGuid(), FathersName = "Gary", MothersName = "Jan", Name="Derek"},
+				new SimpleObject{ Id = Guid.NewGuid(), FathersName = "Murray", MothersName = "Dianne", Name="Eric"},
+				new SimpleObject{ Id = Guid.NewGuid(), FathersName = "Charles", MothersName = "Sue", Name="Justin"},
+				new SimpleObject{ Id = Guid.NewGuid(), FathersName = "Fred", MothersName = "Kathy", Name="Mike"}
+			};
+
+			result = data.ToDictionary(x => x.Id, x => x);
+
+			Response.ContentType = "application/json";
+			return JsonConvert.SerializeObject(result);
+		}
+
+
+		//Part 1
+		public ActionResult Simple()
+		{
+			return View();
+		}
+		[HttpGet]
+		public JsonResult SimpleData()
+		{
+			return Json(new TeamViewModel
+			{
+				Id = Guid.NewGuid(),
+				CurrentReleaseName = "Black Sabbath",
+				ProductName = "Sales Process Pro",
+				TeamMembers = new List<TeamMemberViewModel>
 					 {
 						  new TeamMemberViewModel { Id = Guid.NewGuid(), Name = "Matthew Lee", PreferredLanguages = new List<string> { "JavaScript", "C#" }},
 						  new TeamMemberViewModel { Id = Guid.NewGuid(), Name = "John Najam", PreferredLanguages = new List<string> { "C#", "SQL" }},
@@ -37,34 +95,35 @@ namespace MappingPluginLesson.Controllers
 						  new TeamMemberViewModel { Id = Guid.NewGuid(), Name = "Elyse Dequnia", PreferredLanguages = new List<string> { "CSS" }},
 						  new TeamMemberViewModel { Id = Guid.NewGuid(), Name = "Ryan Conrad", PreferredLanguages = new List<string> { "C#", "JavaScript" }}
 					 }
-				}, JsonRequestBehavior.AllowGet);
-		  }
-		  [HttpPost]
-		  public ActionResult SaveSimpleData(TeamViewModel model)
-		  {
-				return RedirectToAction("Simple");
-		  }
+			}, JsonRequestBehavior.AllowGet);
+		}
+		[HttpPost]
+		public ActionResult SaveSimpleData(TeamViewModel model)
+		{
+			return RedirectToAction("Simple");
+		}
 
-		  //Part 2
-		  public ActionResult UsingCreate(){
-				return View();
-		  }
-		  public ActionResult SaveUsingCreate(TeamViewModel model)
-		  {
-				return RedirectToAction("UsingCreate");
-		  }
+		//Part 2
+		public ActionResult UsingCreate()
+		{
+			return View();
+		}
+		public ActionResult SaveUsingCreate(TeamViewModel model)
+		{
+			return RedirectToAction("UsingCreate");
+		}
 
-		  //Part 3
-		  public ActionResult MVVM()
-		  {
-				return View();
-		  }
+		//Part 3
+		public ActionResult MVVM()
+		{
+			return View();
+		}
 
-		  //Part 4
-		  public ActionResult AppStructure()
-		  {
-				return View();
-		  }
+		//Part 4
+		public ActionResult AppStructure()
+		{
+			return View();
+		}
 
-	 }
+	}
 }
---
+++ MappingPluginLesson/Models/Serialization/SimpleObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MappingPluginLesson.Models.Serialization
+{
+	//Simple
+	public class SimpleObject
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string MothersName { get; set; }
+		public string FathersName { get; set; }
+	}
+	
+	
+
+	//Nested
+	public class Person
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public Person Mother { get; set; }
+		public Person Father { get; set; }
+	}
+}
